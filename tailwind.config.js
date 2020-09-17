258f9fd581eefa7ab7af5434b03402871ea0c19a
Jinyoung <roghabo@gmail.com>
2020-09-17 11:46:11 +0900

    use tailwindcss and add messages

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,19 @@
 module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh",
+      },
+      borderRadius: {
+        xl: "1.5rem",
+      },
+    },
   },
   variants: {},
   plugins: [],
--- users/views.py
+++ users/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.shortcuts import redirect, reverse
 from django.contrib.auth import authenticate, login, logout
 from django.core.files.base import ContentFile
+from django.contrib import messages
 from . import forms, models
 
 
@@ -24,6 +25,7 @@ class LoginView(FormView):
 
 
 def log_out(request):
+    messages.info(request, f"See you later")
     logout(request)
     return redirect(reverse("core:home"))
 
@@ -33,7 +35,6 @@ class SignUpView(FormView):
     template_name = "users/signup.html"
     form_class = forms.SignUpForm
     success_url = reverse_lazy("core:home")
-    initial = {"first_name": "Nicoas", "last_name": "Serr", "email": "itn@las.com"}
 
     def form_valid(self, form):
         form.save()
@@ -84,7 +85,7 @@ def github_callback(request):
             token_json = token_request.json()
             error = token_json.get("error", None)
             if error is not None:
-                raise GithubException()
+                raise GithubException("Can't get access token")
             else:
                 access_token = token_json.get("access_token")
                 profile_request = requests.get(
@@ -103,7 +104,9 @@ def github_callback(request):
                     try:
                         user = models.User.objects.get(email=email)
                         if user.login_method != models.User.LOGIN_GITHUB:
-                            raise GithubException()
+                            raise GithubException(
+                                f"Please log in with: {user.login_method}"
+                            )
                     except models.User.DoesNotExist:
                         user = models.User.objects.create(
                             email=email,
@@ -116,13 +119,14 @@ def github_callback(request):
                         user.set_unusable_password()
                         user.save()
                     login(request, user)
+                    messages.success(request, f"Welcome back {user.first_name}")
                     return redirect(reverse("core:home"))
                 else:
-                    raise GithubException()
+                    raise GithubException("Can't get your profile")
         else:
-            raise GithubException()
-    except GithubException:
-        # send error message
+            raise GithubException("Can't get code")
+    except GithubException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
 
 
@@ -149,41 +153,41 @@ def kakao_callback(request):
         token_json = token_request.json()
         error = token_json.get("error", None)
         if error is not None:
-            raise KakaoException
+            raise KakaoException("Can't get authorization code.")
         access_token = token_json.get("access_token")
-        print(access_token)
         profile_request = requests.get(
-            "https://kapi.kakao.com/v2/user/me",
+            "https://kapi.kakao.com/v1/user/me",
             headers={"Authorization": f"Bearer {access_token}"},
         )
         profile_json = profile_request.json()
-        kakao_account = profile_json.get("kakao_account")
-        email = kakao_account.get("email")
+        email = profile_json.get("kaccount_email", None)
         if email is None:
-            raise KakaoException()
-        profile = kakao_account.get("profile")
-        nickname = profile.get("nickname")
-        profile_image_url = profile.get("profile_image_url")
+            raise KakaoException("Please also give me your email")
+        properties = profile_json.get("properties")
+        nickname = properties.get("nickname")
+        profile_image = properties.get("profile_image")
         try:
             user = models.User.objects.get(email=email)
-            if user.login_method != models.User.LOGIN_KAKAO:
-                raise KakaoException()
+            if user.login_method != models.User.LOGING_KAKAO:
+                raise KakaoException(f"Please log in with: {user.login_method}")
         except models.User.DoesNotExist:
             user = models.User.objects.create(
                 email=email,
                 username=email,
                 first_name=nickname,
-                login_method=models.User.LOGIN_KAKAO,
+                login_method=models.User.LOGING_KAKAO,
                 email_verified=True,
             )
             user.set_unusable_password()
             user.save()
-            if profile_image_url is not None:
-                photo_request = requests.get(profile_image_url)
+            if profile_image is not None:
+                photo_request = requests.get(profile_image)
                 user.avatar.save(
                     f"{nickname}-avatar", ContentFile(photo_request.content)
                 )
+        messages.success(request, f"Welcome back {user.first_name}")
         login(request, user)
         return redirect(reverse("core:home"))
-    except KakaoException:
+    except KakaoException as e:
+        messages.error(request, e)
         return redirect(reverse("users:login"))
